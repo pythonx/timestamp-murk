07c18c23d99986b8af25797899267aee011a1712
Murali Kumar <muralikumar2005@gmail.com>
2018-02-11 16:52:12 +0530

    modified index

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+var app = express();
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+app.get("/", function(req, res)
+
+{
+
+	res.end("hey there ..");
+
+}
+	);
+
+
+
+
